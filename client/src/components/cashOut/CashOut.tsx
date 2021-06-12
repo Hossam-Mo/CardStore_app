c4f47a82049b0f34e8acb36fb928915d8d144662
hossamUK <hossam.m.pro@gmail.com>
2021-06-12 06:07:50 -0700

    cashout

--- client/src/components/cashOut/CashOut.tsx
+++ client/src/components/cashOut/CashOut.tsx
@@ -1,12 +1,20 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import "./cashOut.css";
 import Radio from "@material-ui/core/Radio";
-import { GrMoney } from "react-icons/gr";
+import { GiReceiveMoney } from "react-icons/gi";
 import { GiMoneyStack } from "react-icons/gi";
 import { GiTakeMyMoney } from "react-icons/gi";
 import { useState } from "react";
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
+
+declare const window: any;
+
+const PayPalButton = window.paypal?.Buttons.driver("react", {
+  React,
+  ReactDOM,
+});
 
 export default function CashOut() {
   const [charge, setCharge] = useState<string | undefined>(undefined);
@@ -18,13 +26,42 @@ export default function CashOut() {
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setCharge(event.target.value);
   };
+
+  const createOrder = (data: any, actions: any) => {
+    console.log(data);
+    return actions.order.create({
+      purchase_units: [
+        {
+          description: "Add for the app",
+          amount: {
+            value: "100",
+          },
+        },
+      ],
+    });
+  };
+
+  const onApprove = (data: any, actions: any) => {
+    console.log(data);
+    actions.order
+      .capture()
+      .then((r: any) => {
+        console.log(r);
+      })
+      .catch((err: any) => {
+        console.log(err);
+        alert(err);
+      });
+  };
   return (
     <div className="cashOut">
-      <div className="cashOut_balance">Your Balance: {userInfo.balance} JD</div>
+      <div className="cashOut_balance">
+        Your Balance: {userInfo?.balance} JD
+      </div>
       <div className="cashOut_main">
         <div className="cashOut_card">
           <div className="cashOut_icons">
-            <GrMoney></GrMoney>
+            <GiReceiveMoney></GiReceiveMoney>
             <p>1 JD</p>
           </div>
 
@@ -64,6 +101,10 @@ export default function CashOut() {
             color="primary"
           />
         </div>
+        <PayPalButton
+          createOrder={(data: any, actions: any) => createOrder(data, actions)}
+          onApprove={(data: any, actions: any) => onApprove(data, actions)}
+        />
       </div>
     </div>
   );
