a5730a68cb66cf808c5b6924eb4941523a145591
hossamUK <hossam.m.pro@gmail.com>
2021-06-12 05:48:00 -0700

    cashout

--- client/src/components/cashOut/CashOut.tsx
+++ client/src/components/cashOut/CashOut.tsx
@@ -1,5 +1,70 @@
 import React from "react";
 import "./cashOut.css";
+import Radio from "@material-ui/core/Radio";
+import { GrMoney } from "react-icons/gr";
+import { GiMoneyStack } from "react-icons/gi";
+import { GiTakeMyMoney } from "react-icons/gi";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+
 export default function CashOut() {
-  return <div>hello</div>;
+  const [charge, setCharge] = useState<string | undefined>(undefined);
+  const userInfo = useSelector((state: any) => state.userInfo);
+
+  useEffect(() => {
+    console.log(userInfo);
+  }, [userInfo]);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCharge(event.target.value);
+  };
+  return (
+    <div className="cashOut">
+      <div className="cashOut_balance">Your Balance: {userInfo.balance} JD</div>
+      <div className="cashOut_main">
+        <div className="cashOut_card">
+          <div className="cashOut_icons">
+            <GrMoney></GrMoney>
+            <p>1 JD</p>
+          </div>
+
+          <Radio
+            checked={charge === "1"}
+            onChange={handleChange}
+            value="1"
+            name="chargeButton"
+            color="primary"
+          />
+        </div>
+        <div className="cashOut_card">
+          <div className="cashOut_icons">
+            <GiMoneyStack></GiMoneyStack>
+            <p>10 JD</p>
+          </div>
+
+          <Radio
+            checked={charge === "10"}
+            onChange={handleChange}
+            value="10"
+            name="chargeButton"
+            color="primary"
+          />
+        </div>
+        <div className="cashOut_card">
+          <div className="cashOut_icons">
+            <GiTakeMyMoney></GiTakeMyMoney>
+            <p>50 JD</p>
+          </div>
+
+          <Radio
+            checked={charge === "50"}
+            onChange={handleChange}
+            value="50"
+            name="chargeButton"
+            color="primary"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
