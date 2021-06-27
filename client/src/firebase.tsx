09e3d511735df9031284b7ebce07dcc27b2ba373
hossamUK <hossam.m.pro@gmail.com>
2021-06-27 06:38:47 -0700

    hosting

--- client/src/firebase.tsx
+++ client/src/firebase.tsx
@@ -1,6 +1,6 @@
 import firebase from "firebase";
 
-const firebaseConfig = {
+/* const firebaseConfig = {
   apiKey: "AIzaSyCgvacOwHJxTKGZLqqjn3KTckyEAhKkQkU",
   authDomain: "yasserproject-826dd.firebaseapp.com",
   projectId: "yasserproject-826dd",
@@ -8,6 +8,16 @@ const firebaseConfig = {
   messagingSenderId: "940656393374",
   appId: "1:940656393374:web:be1bf491e01e35eaf28f8f",
   measurementId: "G-6V8ZPDF0SQ",
+}; */
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDvmzbU1YP0lSTpV2mTwcqNSWgsYFfEQUw",
+  authDomain: "cardapp-jos.firebaseapp.com",
+  projectId: "cardapp-jos",
+  storageBucket: "cardapp-jos.appspot.com",
+  messagingSenderId: "938253126099",
+  appId: "1:938253126099:web:d777b88b87dccbc3f88726",
+  measurementId: "G-0SNML9YKHD",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
